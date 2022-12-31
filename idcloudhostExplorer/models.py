5d23cc37e0b2e7b521d976d380e1e0ab8cd27f41
Kristian Alexander P <alexforsale@yahoo.com>
2022-12-31 08:37:14 +0000

    update idcloudhostExplorer

--- idcloudhostExplorer/models.py
+++ idcloudhostExplorer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class CloudHost(models.Model):
+    cookie_id = models.CharField(max_length=56)
+    id = models.IntegerField()
+    name = models.EmailField()
+    first_name = models.CharField(max_length=30)
